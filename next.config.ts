7fbc9d76c557e1ea3539cccfc0e29e0a18d6278c
yadeggss <harunbashir0@gmail.com>
2026-07-08 00:45:30 +0100

    fix: next.config serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@shelby-protocol/sdk"],
-  },
+  serverExternalPackages: ["@shelby-protocol/sdk"],
 };
 
 export default nextConfig;
